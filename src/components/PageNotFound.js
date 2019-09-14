a6c48bbc361c6f11c94ae7dacda3c499913d4da3
w4wycliff <w4wycliff@gmail.com>
2019-09-14 11:55:20 +0300

    setting up

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const PageNotFound = () => <h1>Ooops! Page not founc</h1>
+export default PageNotFound;
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AboutPage = () => (
+<div>
+    <h1>About</h1>
+    <p>The app uses react</p>
+</div>
+);
+export default AboutPage;
